1c7d3d3ab86a5a452f369f32794cf9cab06a2cb3
devopsevd <devopsevd@users.noreply.github.com>
2018-09-12 01:01:01 +0530

    Update TestWithLiquibase.java

--- src/test/java/net/javaforge/blog/liquibase/TestWithLiquibase.java
+++ src/test/java/net/javaforge/blog/liquibase/TestWithLiquibase.java
@@ -31,7 +31,7 @@ public class TestWithLiquibase {
 		//conn = DriverManager.getConnection("jdbc:h2:liquibase-sample", "sa",
 		//		"sa");
 		Class.forName("org.postgresql.Driver");
-		conn = DriverManager.getConnection("jdbc:h2://localhost:5432", "demo",
+		conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432", "demo",
 				"demo_pass");
 
 		Database database = DatabaseFactory.getInstance()
